14ded94e42a8b19294ce28f6d6a5486a8ee512fb
MH TOUFIK <hello@toufikforyou.dev>
2026-08-05 22:39:19 +0600

    Initial Commit

---
+++ BooksMyFriend/BooksMyFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksMyFriendApp.swift
+//  BooksMyFriend
+//
+//  Created by MD TOUFIK HASAN on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BooksMyFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BooksMyFriendTests/BooksMyFriendTests.swift
@@ -0,0 +1,18 @@
+//
+//  BooksMyFriendTests.swift
+//  BooksMyFriendTests
+//
+//  Created by MD TOUFIK HASAN on 5/8/26.
+//
+
+import Testing
+
+struct BooksMyFriendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
